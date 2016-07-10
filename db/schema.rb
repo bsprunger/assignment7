f05352d91c86f01042206a6d070fdeb6beb6c690
Ben Sprunger <benj.sprunger@gmail.com>
2016-07-10 19:04:31 +0000

    schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20111119180638) do
-  
-  enable_extension "plpgsql"
+ActiveRecord::Schema.define(version: 0) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+  
   create_table "movies", force: :cascade do |t|
     t.string   "title"
     t.string   "rating"
