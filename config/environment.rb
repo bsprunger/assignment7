007cf2c76bd55213d4ae0f8dab370a7ba9c6c983
Ben Sprunger <benj.sprunger@gmail.com>
2016-07-10 16:51:59 +0000

    working locally

--- config/environment.rb
+++ config/environment.rb
@@ -2,6 +2,7 @@
 require File.expand_path('../application', __FILE__)
 
 ::ActiveSupport::Deprecation.silenced = true if Rails.env.test?
+::ActiveSupport::Deprecation.silenced = true if Rails.env.production?
 
 # Initialize the rails application
 Rottenpotatoes::Application.initialize!
