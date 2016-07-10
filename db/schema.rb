1b3ab80219aab590ac0991e41c75376c8a0b60b6
Ben Sprunger <benj.sprunger@gmail.com>
2016-07-10 16:40:37 +0000

    updated schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,8 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20111119180638) do
+  
+  enable_extension "plpgsql"
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
